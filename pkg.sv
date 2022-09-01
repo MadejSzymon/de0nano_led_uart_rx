5f7b7e8b6bf7fb3d4538b0dea66f049c44302980
MadejSzymon <110915050+MadejSzymon@users.noreply.github.com>
2022-09-01 11:20:22 +0200

    Add files via upload

--- pkg.sv
+++ pkg.sv
@@ -2,7 +2,9 @@ package pkg;
 	
 	parameter DATA_BITS = 8;
 	parameter STOP_BITS = 1;
-	parameter TICK_NBR = 5208; //TICK_NBR = floor(clock_frequency/baud_rate) : floor(25_000_000/9600) = 2604
+	parameter integer CLK_FREQ = 50_000_000;
+	parameter integer BAUD_RATE = 9600;
+	parameter integer TICK_NBR = CLK_FREQ/BAUD_RATE;
 	
 	typedef enum {RX_IDLE,RX_START,RX_DATA,RX_STOP} rx_state;
 	
